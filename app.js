08d22255183cd1f22b8a189e76617d70555c9260
vipyne <vipyne@gmail.com>
2014-07-26 23:56:42 -0400

    Add email and password validation.
    more elegant error handling (i think) than first pass.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function(window, document, undfined){ // trying to stay out of global scope
+(function(window, document, undefined){ // trying to stay out of global scope
 
 // placeholder //////////////
 /////////////////////////////
@@ -19,9 +19,9 @@ var placeholder = (function(){
   }
 
   var grey = function(){
-    var inputs = document.querySelectorAll('.grey')
-    for(var i = 0; i < inputs.length; i++){
-      inputs[i].style.color = 'rgba(170,170,170,1)'
+    var placeholders = document.querySelectorAll('.grey')
+    for(var i = 0; i < placeholders.length; i++){
+      placeholders[i].style.color = 'rgba(170,170,170,1)'
     }
   }
 
@@ -63,24 +63,18 @@ var validate = (function(){
 
   var form = document.querySelectorAll('.validate')[0]
 
-  var errorHandler = function(message){
-    var oldErrorP = document.getElementsByTagName('p')
-    var errorP = document.createElement('p')
-    var content = document.getElementById('content')
-    var DOMContents = document.getElementById('content').children
-    while(oldErrorP[0]) oldErrorP[0].parentNode.removeChild(oldErrorP[0]) // incase user retry login
-    errorP.innerHTML = message
-    content.appendChild(errorP)
-    content.insertBefore(DOMContents[2], DOMContents[1])
-  }
-
-  var emailInput = function(email){
+  var email = function(){
+    var email = document.querySelectorAll('.email-require')[0].value
     var pattern = /^(?!\.)[\w\W]+@[\w\W]+\.[\w\W]{3}$/g // TODO - address if '.' is before '@'
-    pattern.lastIndex = 0 // tests string from beginning again if user retry login
-    return pattern.test(email)
+    if(email.search(pattern) == -1){
+      return 'email'
+    }else{
+      return null // passed validation
+    }
   }
 
-  var passwordInput = function(password){
+  var password = function(){
+    var password = document.querySelectorAll('.password-require')[0].value
     if(password.search(/\d/g) == -1){
       return 'no number'
     }else if(password.search(/[A-Z]/g) == -1){
@@ -88,40 +82,49 @@ var validate = (function(){
     }else if(password.search(/[a-z]/g) == -1){
       return 'no lowercase'
     }else{
-      return false
+      return null // passed validation
     }
   }
 
   var message = function(input){
     var err
-    var errors = {  // change to object literal?
-      'email': 'sorry, that email is not valid',
+    var errors = {
+      'email': 'sorry, that email address is not valid',
       'no number': 'please include at least one number in your password',
       'no uppercase': 'please include at least one uppercase character in your password',
       'no lowercase': 'please include at least one lowercase character in your password',
-      'default': 'opps, something went wrong! please reload page and try again'
+      'none': null // passed validation
     }
     if(errors[input]){
       err = errors[input]
     }else{
-      err = errors['default']
+      err = errors['none']
     }
     return err
   }
 
-  var formInput = function(event){
-    var email = document.querySelectorAll('.email-require')[0].value
-    var password = document.querySelectorAll('.password-require')[0].value
-    if(!emailInput(email)){ // check email
-      event.preventDefault()
-      errorHandler(message('email'))
-    }else if(passwordInput(password)){ // check password
-      event.preventDefault()
-      errorHandler(message(passwordInput(password))) // i know this is ugly and inefficient
+  var error = function(message){
+    if(message == null){
+      return false // passed validation
+    }else{
+      var oldErrorP = document.getElementsByTagName('p')
+      var errorP = document.createElement('p')
+      var content = document.getElementById('content')
+      var DOMContents = document.getElementById('content').children
+      while(oldErrorP[0]) oldErrorP[0].parentNode.removeChild(oldErrorP[0]) // incase user retry login
+      errorP.innerHTML = message
+      content.appendChild(errorP)
+      content.insertBefore(DOMContents[2], DOMContents[1])
+      return true // error message passed to DOM
     }
   }
 
-  // here too, realized most of these don't need to be public
+  var formInput = function(event){
+    if(error( message( email() )) || error( message( password() ))){
+      event.preventDefault()
+    }
+  }
+
   return {
     form: form,
     formInput: formInput
@@ -135,18 +138,14 @@ var validate = (function(){
 /////////////////////////////
 /////////////////////////////
 
-// determine brower compatibility (attachEvent vs addEventListener)
+// determine browser compatibility (attachEvent vs addEventListener)
 
 if(validate.form.addEventListener){
-  // memory inefficient but anonymous function works for now
-  // need to bind this (or something) maybe better this way because
-  // more compatible with older browswers
   validate.form.addEventListener('submit', function(event){
     validate.formInput(event)
   }, false)
-
   }else if(validate.form.attachEvent){
-    placeholder().IEcompatible()
+    placeholder.IEcompatible()
     validate.form.attachEvent('onsubmit', function(event){
       validate.formInput(event)
     }, false)
